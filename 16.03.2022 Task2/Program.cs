c6ff48bd5fc540ec933f9d14cd0ebdfef0138575
evezmemmedli <avazkm@code.edu.az>
2022-03-15 22:56:22 +0400

    Add project files.

---
+++ 16.03.2022 Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace _16._03._2022_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sortedList = new SortedList();
+            sortedList.Add(1985,"C++");
+            sortedList.Add(1984, "Python");
+            sortedList.Add(1995, "C#");
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine(item.Key +" "+ item.Value);
+            }
+            SortedList sortedList1 = new SortedList();
+            sortedList1.Add("Python", 1984);
+            sortedList1.Add("A#",1995);
+            sortedList1.Add("C++",1985);
+            foreach (DictionaryEntry item in sortedList1)
+            {
+                Console.WriteLine(item.Value + " " + item.Key);
+            }
+        }
+    }
+}
